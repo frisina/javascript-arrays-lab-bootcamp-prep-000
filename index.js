feac64e1952b3d0889d4c822aea6381e9a6663af
Marco Frisina <marco@frisina.xyz>
2019-05-03 12:28:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ['Milo', 'Otis', 'Garfield']; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  push.kittens(name);
+  kittens = push.kittens(name);
   return kittens;
 }
