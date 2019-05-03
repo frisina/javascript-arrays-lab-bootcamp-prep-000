395e55c6a7d08ca42fc5b2aef6b112febc32b43e
Marco Frisina <marco@frisina.xyz>
2019-05-03 12:31:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
 }
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
